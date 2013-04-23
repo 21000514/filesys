9cde648437ea80a92bec3df4c7e53b5e574c5943
Rasamoela <gilberto.rasamoela@laposte.net>
2013-04-23 16:48:43 +0200

    Ajout du JUnitTest vérifiant les répertoires

---
+++ filesys/src/test/java/testfilesys/TestRepertoire.java
@@ -0,0 +1,96 @@
+package testfilesys;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Exceptions.AddNullException;
+import Exceptions.FichierTailleNegativeException;
+import Exceptions.IsInHimselfException;
+import Exceptions.NomExistePasException;
+import filesys.Repertoire;
+import filesys.Entite;
+import filesys.Fichier;
+
+/** 
+ * Classe TestRepertoire testant les méthodes de la classe répertoire  
+ * 
+ *  @author RASAMOELA Gilberto, HAMEUR Youssef 
+ *  @version 23/04/13 
+ */
+public class TestRepertoire {
+
+	/** 
+	 * Teste qu'un répertoire créé ne renvoie pas une référence null
+	 * 
+	 */
+	@Test
+	public void testRepertoire() {
+		Repertoire r = new Repertoire();
+		assertTrue(r.getType() != null);
+	}
+
+	/** 
+	 * Teste que l'ajout dans un répertoire ne peut se faire dans lui-même
+	 * 
+	 */
+	@Test
+	public void testAdd() {
+		Repertoire pere = new Repertoire();
+		Repertoire r1 = new Repertoire("r1");
+
+		try {
+			pere.add(r1);
+		} catch (AddNullException e) {
+			e.printStackTrace();
+		} catch (NomExistePasException e) {
+			e.printStackTrace();
+		} catch (IsInHimselfException e) {
+			e.printStackTrace();
+		}
+		assertFalse(pere.equals(r1));
+	}
+	/** 
+	 * Test de la méthode de détection qu'un répertoire est dans lui-même
+	 * 
+	 */
+	@Test
+	public void testIsInHimself() {
+		Repertoire r1 = new Repertoire("r1");
+		try {
+			r1.add(r1);
+		} catch (AddNullException e1) {
+			e1.printStackTrace();
+		} catch (NomExistePasException e1) {
+			e1.printStackTrace();
+		} catch (IsInHimselfException e1) {
+			e1.printStackTrace();
+		}
+		assertTrue(r1.isInHimself(r1));
+	}
+	/** 
+	 * Teste qu'un répertoire ajouté n'a pas le même nom qu'un répertoire déjà présent
+	 * 
+	 */
+	@Test
+	public void testNomExistePas() {
+		Repertoire r1 = new Repertoire();
+		Repertoire r2 = new Repertoire("r1");
+		Repertoire r3 = new Repertoire("r1");
+		
+		try {
+			r1.add(r2);
+		} catch (AddNullException e) {
+			e.printStackTrace();
+		} catch (NomExistePasException e) {
+			e.printStackTrace();
+		} catch (IsInHimselfException e) {
+			e.printStackTrace();
+		}
+		for (Entite e : r1.getContenu())
+		{
+		assertTrue(r1.getNom() != e.getNom());
+		}
+		
+	}
+
+}
