592861cf113d89e1f84caf3d347b8187fca8368f
Rasamoela <gilberto.rasamoela@laposte.net>
2013-04-23 17:00:59 +0200

    Modification TestFichier

--- filesys/src/test/java/testfilesys/TestFichier.java
+++ filesys/src/test/java/testfilesys/TestFichier.java
@@ -8,12 +8,24 @@ import Exceptions.FichierTailleNegativeException;
 
 import filesys.Fichier;
 
+/** 
+ * Classe TestFichier testant les méthodes de la classe fichier  
+ * 
+ *  @author RASAMOELA Gilberto, HAMEUR Youssef 
+ *  @version 23/04/13 
+ */
 public class TestFichier {
 
+	/** 
+	 * Teste qu'un fichier créé ne renvoie pas une référence null
+	 * et qu'il ne soit pas avec une taille négative
+	 * 
+	 */
 	@Test
 	public void testFichier() {
 		try {
 			Fichier f1 = new Fichier("f1",10);
+			assertNotNull(f1);
 			assertTrue(f1.getTaille()>0);
 		} catch (FichierTailleNegativeException e) {
 			e.printStackTrace();
